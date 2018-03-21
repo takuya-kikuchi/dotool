a11a2d8825081eb61370009955ffe7d94a7acad6
takuya kikuchi <t.kikuchi.pch@gmail.com>
2018-03-21 20:57:40 +0900

    Implement a bucket tool

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -12,6 +12,8 @@
             @mouseup="mouseup"
             @mousemove="mousemove"
         ></canvas>
+        <input type="checkbox" id="checkbox" v-model="checked">
+        <label for="checkbox">{{ checked }}</label>
         </div>
     </div>
 </template>
@@ -36,7 +38,8 @@ export default {
       canvasHeight: 240,
       dragging: false,
       previousPoint: {},
-      msg: '0w0'
+      msg: '0w0',
+      checked: false
     }
   },
   methods: {
@@ -76,7 +79,11 @@ export default {
       // } else {
       //   this.addNode(mouseUpPoint)
       // }
-      this.addNode(mouseUpPoint)
+      if (this.checked) {
+        this.fillAsFlood(mouseUpPoint)
+      } else {
+        this.addNode(mouseUpPoint)
+      }
       console.log(`mouseUp! ${mouseUpPoint.x}, ${mouseUpPoint.y}`)
     },
     dragPoint (selectedPoint, newPoint) {
@@ -117,8 +124,80 @@ export default {
       this.context.fill()
       this.context.strokeStyle = this.strokeStyle
       this.context.stroke()
+    },
+    fillAsFloodInternal (point, imgData, baseColor, fillColor, stack) {
+      var y = point.y
+      var x = point.x
+      var pixelPos = (y * this.canvasWidth + point.x) * 4
+      var canvasWidth = this.canvasWidth
+      var canvasHeight = this.canvasHeight
+      while (y-- >= 0 && matchStartColor(pixelPos, imgData, baseColor)) {
+        pixelPos -= canvasWidth * 4
+      }
+      pixelPos += canvasWidth * 4
+      y++
+      var reachLeft = false
+      var reachRight = false
+      while (y++ < canvasHeight - 1 && matchStartColor(pixelPos, imgData, baseColor)) {
+        colorPixel(pixelPos, imgData, fillColor)
+
+        if (x > 0) {
+          if (matchStartColor(pixelPos - 4, imgData, baseColor)) {
+            if (!reachLeft) {
+              stack.push({x: x - 1, y: y})
+              reachLeft = true
+            }
+          } else {
+            reachLeft = false
+          }
+        }
+        if (x < canvasWidth - 1) {
+          if (matchStartColor(pixelPos + 4, imgData, baseColor)) {
+            if (!reachRight) {
+              stack.push({x: x + 1, y: y})
+              reachRight = true
+            }
+          } else {
+            reachRight = false
+          }
+        }
+        pixelPos += canvasWidth * 4
+      }
+    },
+    fillAsFlood (point) {
+      var imgData = this.context.getImageData(0, 0, this.canvasWidth, this.canvasHeight)
+      var pixelPos = (point.y * this.canvasWidth + point.x) * 4
+      var baseColor = decompositColor(imgData, pixelPos)
+      console.log(`${baseColor.R} ${baseColor.G} ${baseColor.B}, ${this.fillStyle}`)
+      var stack = [point]
+      while (stack.length > 0) {
+        var current = stack.pop()
+        this.fillAsFloodInternal(current, imgData, baseColor, {R: 255, G: 123, B: 0}, stack)
+      }
+      this.context.putImageData(imgData, 0, 0)
     }
   }
+}
+
+function colorPixel (pixelPos, imgData, baseColor) {
+  imgData.data[pixelPos] = baseColor.R
+  imgData.data[pixelPos + 1] = baseColor.G
+  imgData.data[pixelPos + 2] = baseColor.B
+  imgData.data[pixelPos + 3] = 255
+}
+function matchStartColor (pixelPos, imgData, baseColor) {
+  var r = imgData.data[pixelPos]
+  var g = imgData.data[pixelPos + 1]
+  var b = imgData.data[pixelPos + 2]
+  // console.log(`${r} vs ${baseColor.R}, ${g} vs ${baseColor.G}, ${b} vs ${baseColor.B}`)
+  return (r === baseColor.R && g === baseColor.G && b === baseColor.B)
+}
+
+function decompositColor (imgData, pixelPos) {
+  var r = imgData.data[pixelPos]
+  var g = imgData.data[pixelPos + 1]
+  var b = imgData.data[pixelPos + 2]
+  return { R: r, G: g, B: b }
 }
 </script>
 
